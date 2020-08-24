fc7fdc7c60c6bec6241a6b238f677fe1de120fe7
aadiityaak <aadiityaak@gmail.com>
2020-08-24 17:13:18 +0700

    first commit :)

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+// import { Form, Row, Col} from "react-bootstrap";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          hello world!
-        </p>
-        <a className="btn btn-danger text-white">
-          tombol
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: 'wsvincent'
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      username: e.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div className="container py-5">
+        Hello {this.state.username} <br />
+        Change name:
+        <input type="text" value={this.state.username} onChange={this.handleChange} />
+      </div>
+    );
+  }
 }
 
 export default App;
