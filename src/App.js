1b5c2562acd90a1a773dc384a2a669f5ec8a93aa
aadiityaak <aadiityaak@gmail.com>
2020-08-24 16:40:24 +0700

    init :)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          hello world!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a className="btn btn-danger text-white">
+          tombol
         </a>
       </header>
     </div>
